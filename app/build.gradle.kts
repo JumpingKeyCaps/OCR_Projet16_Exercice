b7ee2b6992529fa85bb40d8851f033019f043fd6
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-29 16:29:04 +0100

    build(CI): - reverse to original way to done the CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -172,9 +172,9 @@ tasks.withType<Test> {
 val androidExtension = extensions.getByType<BaseExtension>()
 
 val jacocoTestReport by tasks.registering(JacocoReport::class) {
-   // dependsOn("testDebugUnitTest","connectedDebugAndroidTest", "createDebugCoverageReport")
+    dependsOn("testDebugUnitTest","connectedDebugAndroidTest", "createDebugCoverageReport")
 
-    dependsOn("testDebugUnitTest")
+
     group = "Reporting"
     description = "Generate Jacoco coverage reports"
 
