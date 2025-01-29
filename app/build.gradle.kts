0a7a42b4e58e98ccb7d1ffd9de2deb3d85d64e17
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-29 06:33:42 +0100

    build(CI): - fix missing debug coverage report

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -174,7 +174,7 @@ val androidExtension = extensions.getByType<BaseExtension>()
 val jacocoTestReport by tasks.registering(JacocoReport::class) {
    // dependsOn("testDebugUnitTest","connectedDebugAndroidTest", "createDebugCoverageReport")
 
-    dependsOn("testDebugUnitTest")
+    dependsOn("testDebugUnitTest","createDebugCoverageReport")
     group = "Reporting"
     description = "Generate Jacoco coverage reports"
 
